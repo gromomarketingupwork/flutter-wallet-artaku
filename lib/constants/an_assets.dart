a1812370cc5ba88638d5250e5a1f934f87f058ef
gromomarketingupwork <gromomarketing.upwork@gmail.com>
2021-05-06 10:00:54 +0545

    UI/UX refinement complete

--- lib/constants/an_assets.dart
+++ lib/constants/an_assets.dart
@@ -5,5 +5,5 @@ class ANAssets{
     static const String profileImage = 'assets/icons/profile.png';
     static const String walletIcon = 'assets/icons/wallet_icon.png';
     static const String placeHolderImage = 'assets/icons/placeholder_image.png';
-
+    static const String faceIdIcon = 'assets/icons/face_id_icon.png';
 }
--- lib/screens/pin_set_page.dart
+++ lib/screens/pin_set_page.dart
@@ -1,38 +1,47 @@
 import 'package:etherwallet/components/appbar/an_appbar.dart';
 import 'package:etherwallet/components/button/an_button.dart';
 import 'package:etherwallet/components/form/an_text_field.dart';
+import 'package:etherwallet/constants/an_assets.dart';
 import 'package:etherwallet/constants/colors.dart';
 import 'package:etherwallet/constants/syles.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class PinSetPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _PinSetPageState();
-
 }
 
 class _PinSetPageState extends State<PinSetPage> {
   bool biometricSwitch = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ANColor.primary,
-      appBar: ANAppBar(appBar: AppBar(),),
+      appBar: ANAppBar(
+        appBar: AppBar(),
+      ),
       body: Align(
         alignment: Alignment.center,
         child: Column(
           children: [
-            SizedBox(height: 32,),
-            Text("Secure Your Wallet",
-              style: header1.copyWith(color: ANColor.backgroundText),),
-            SizedBox(height: 32,),
+            SizedBox(
+              height: 32,
+            ),
+            Text(
+              "Secure Your Wallet",
+              style: header1.copyWith(color: ANColor.backgroundText),
+            ),
+            SizedBox(
+              height: 32,
+            ),
             ANTextField(
               hintText: "123456",
               width: 150,
             ),
-            SizedBox(height: 32,),
-
+            SizedBox(
+              height: 32,
+            ),
             ANButton(
               label: "Set Pin",
               width: 150,
@@ -48,8 +57,15 @@ class _PinSetPageState extends State<PinSetPage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Icon(FontAwesomeIcons.male),
-                  Text("Biometrics Enabled", style: header2.copyWith(color: ANColor.backgroundText),),
+                  Image.asset(
+                    ANAssets.faceIdIcon,
+                    height: 50,
+                    width: 50,
+                  ),
+                  Text(
+                    "Biometrics Enabled",
+                    style: header2.copyWith(color: ANColor.backgroundText),
+                  ),
                   Transform.scale(
                     scale: 1.4,
                     child: Switch(
@@ -59,19 +75,16 @@ class _PinSetPageState extends State<PinSetPage> {
                           biometricSwitch = value;
                         });
                       },
-                      activeTrackColor: Color(0xFF1EBD71).withOpacity(0.5),
-                      activeColor: Color(0xFF1EBD71),
+                      activeTrackColor: Color(0xFFC2EFB3).withOpacity(0.5),
+                      activeColor: Color(0xFFC2EFB3),
                     ),
                   )
-
                 ],
               ),
             )
-
           ],
         ),
       ),
     );
   }
-
 }
